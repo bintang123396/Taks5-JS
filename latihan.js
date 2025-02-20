79af14531e4b107e0e0464d545e90a58458ae6e2
bintang123396 <bintangpetikppl@gmail.com>
2025-02-20 20:18:10 +0700

    Add files via upload

---
+++ latihan.js
@@ -0,0 +1,10 @@
+alert("Hello World!");
+// document.write("<h1>Saya Bintang Rudianto</h1>");
+// document.write("<hr>");
+// document.write("<p>Saya sedang belajar Javascript</p>");
+// document.write("di <b>petanikode.com</b>")
+
+//===========================================
+
+var nama = prompt("Siapa nama kamu?","");
+document.write("<p>Hello "+ nama +"</p>");
